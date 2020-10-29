2f04dbcce87adfcd2843cfeb8fe9e8557a113cf5
Paul_Mirel <paul.mirel@startmail.com>
2020-10-28 20:44:32 -0400

    add rangefinder code

---
+++ Session_2/TBD-read-rangefinder-on-i2c-bus/code.py
@@ -0,0 +1,14 @@
+import time
+import board
+import busio
+import adafruit_vl53l0x
+
+i2c_bus = busio.I2C( board.SCL, board.SDA )
+rangefinder = adafruit_vl53l0x.VL53L0X( i2c_bus )
+
+while True:
+    distance_mm = rangefinder.range
+    if distance_mm > 1000:
+        distance_mm = 1000
+    print( distance_mm )
+    time.sleep(0.1)
