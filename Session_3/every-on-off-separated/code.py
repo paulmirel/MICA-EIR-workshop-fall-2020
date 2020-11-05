be0d39492bb022e1731390aa52842cbd694f9ca1
Paul_Mirel <paul.mirel@startmail.com>
2020-11-05 16:56:50 -0500

    add two forms of every

---
+++ Session_3/every-on-off-separated/code.py
@@ -0,0 +1,25 @@
+#import libraries
+import time
+import board
+from adafruit_circuitplayground import cp
+from every import Every
+
+#set up
+blink_interval = Every( 1.0 ) #create the blink interval timer
+on_duration = 2.0
+off_duration = 5.0
+
+color = ( 200, 0, 80 )
+OFF = ( 0, 0, 0 )
+
+#loop
+while True:
+    if blink_interval():
+       if cp.pixels[5] == OFF: # if already off, then
+           cp.pixels[ 5 ] = color
+           blink_interval.interval = on_duration # change to the on duration for the next check.
+       else: # if already on, then
+           cp.pixels[ 5 ] = OFF
+           blink_interval.interval = off_duration # change to the off duration for the next check.
+    #slow the loop only in one place
+    time.sleep( 0.01 )
--- Session_3/every-use-template/code.py
+++ Session_3/every-use-template/code.py
@@ -5,8 +5,7 @@ from adafruit_circuitplayground import cp
 from every import Every
 
 #set up
-blink_interval = Every( 5.0 ) #create the blink interval timer
-on_duration = Every( 2.0 )
+blink_interval = Every( 1.0 ) #create the blink interval timer
 
 color = ( 200, 0, 80 )
 OFF = ( 0, 0, 0 )
@@ -14,9 +13,9 @@ OFF = ( 0, 0, 0 )
 #loop
 while True:
     if blink_interval():
-        cp.pixels[ 5 ] = color
-    if on_duration():
-        cp.pixels[ 5 ] = OFF
+       if cp.pixels[5] == OFF: # if already off, then
+           cp.pixels[ 5 ] = color
+       else: # if already on, then
+           cp.pixels[ 5 ] = OFF
     #slow the loop only in one place
     time.sleep( 0.01 )
-
